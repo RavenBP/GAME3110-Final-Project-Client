07871f108575b1da11d6ed10c505a29eef0a73e4
RavenBP <ravenbpowless@gmail.com>
2020-10-10 19:16:23 -0400

    added simple unity project
    - added simple unity project with some UI elements
    - some functionality added to buttons/screens
    - created UI script

---
+++ GAME3110 Final Project Client/Assets/_Scripts/PlayerInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    string username;
+    //string password; // Maybe it might be better to just have this stored on the server somewhere?
+    int numWins; // Number of wins that can be presented to the player in their account info, this could also be used to calculate their level
+    int playerLevel; // This could increase every 100xp for example
+    int xpRequired = 100; // Amount of xp needed to level up
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    string GetUsername()
+    {
+        return username;
+    }
+
+    int GetNumWins()
+    {
+        return numWins;
+    }
+
+    int GetXPRequired()
+    {
+        return xpRequired;
+    }
+
+    int GetPlayerLevel()
+    {
+        return playerLevel;
+    }
+}
---
+++ GAME3110 Final Project Client/Assets/_Scripts/UI.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public TMP_InputField tmpInputField;
+    public GameObject errorText; // NOTE: Would be better to specify that this is a TMP Text object...
+    string username; // TODO: Associate this with a client
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    ///////////////////////////////////// LoginScene /////////////////////////////////////
+
+    public void Login()
+    {
+        if (username == "test") // TODO: Check to see if a client with this username exists (maybe inside a list?)
+        {
+            SceneManager.LoadScene("GameScene"); // Load Game Scene
+        }
+        else // Display error text
+        {
+            Debug.Log("Username does not exist.");
+            errorText.SetActive(true);
+        }
+    }
+
+    public void EnteredUsername()
+    {
+        username = tmpInputField.text;
+        //Debug.Log("The current value of input field is: " + username);
+    }
+
+    public void CreateAccount()
+    {
+        // Either load a scene or a pop up panel?
+    }
+
+
+
+    ///////////////////////////////////// LoginScene /////////////////////////////////////
+
+    ///////////////////////////////////// GameScene /////////////////////////////////////
+
+    public void SubmitLetter()
+    {
+        Debug.Log("Player entered the letter: " + tmpInputField.text);
+    }
+
+    public void ViewAccount()
+    {
+        SceneManager.LoadScene("AccountScene");
+    }
+
+    public void Logout()
+    {
+        // NOTE: Will likely need to remove all variables associated with the current user...
+
+        SceneManager.LoadScene("LoginScene");
+    }
+
+    ///////////////////////////////////// GameScene /////////////////////////////////////
+
+    ///////////////////////////////////// DEBUG /////////////////////////////////////
+
+    public void LoadLoginScene()
+    {
+        SceneManager.LoadScene("LoginScene");
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadAccountScene() // Also used inside GameScene
+    {
+        SceneManager.LoadScene("AccountScene");
+    }
+
+    ///////////////////////////////////// DEBUG /////////////////////////////////////
+}
