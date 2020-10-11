b8042d07fcbb1e8817fbd3a3c39ce5540f650061
kossam001 <kossam001@gmail.com>
2020-10-11 14:45:39 -0400

    Screen shows tiles with letters on them
    Try to get the Wheel of Fortune vibe by showing the hidden phrase as a series of panels.

---
+++ GAME3110 Final Project Client/Assets/_Scripts/Display.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Display : MonoBehaviour
+{
+    public Dictionary<char, List<int>> charToIndexDict; // Maps letters to location in phrase using indices
+    private string alphabet = "abcdefghijklmnopqrstuvwxyz "; // Alphabet + White space
+
+    public string originalPhrase;
+    private char[] solution;
+    private char[] guesses;
+
+    // Panels
+    public List<List<GameObject>> panelLayout; // How to display to players
+    public List<GameObject> solutionPanels; // How the game sees the panels
+
+    [SerializeField]
+    private GameObject letterPrefab;
+
+    public Guess guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (int index in charToIndexDict[guess.guess])
+        {
+            solutionPanels[index].GetComponentInChildren<TextMeshProUGUI>().enabled = true;
+        }
+    }
+
+    private void Setup()
+    {
+        // Setting up arrays
+        originalPhrase = originalPhrase.ToUpper();
+        alphabet = alphabet.ToUpper();
+
+        solution = originalPhrase.ToCharArray();
+        guesses = new char[solution.Length];
+        solutionPanels = new List<GameObject>();
+        panelLayout = new List<List<GameObject>>();
+        charToIndexDict = new Dictionary<char, List<int>>();
+
+        InitPanelLayout();
+        PresentPanel();
+
+
+
+        // Init dictionary
+        foreach (char letter in alphabet)
+        {
+            charToIndexDict[letter] = new List<int>();
+        }
+
+        for (int i = 0; i < originalPhrase.Length; i++)
+        {
+            // Find the dictionary entry and add the index
+            // The key is the alphabet
+            charToIndexDict[originalPhrase[i]].Add(i);
+        }
+    }
+
+    void InitPanelLayout()
+    {
+        string[] wordList = originalPhrase.Split();
+        List<List<GameObject>> wordPanels = new List<List<GameObject>>();
+
+        // Init Panels
+        // Create panel references from game logic to onscreen display
+        foreach (string word in wordList)
+        {
+            List<GameObject> wordPanel = new List<GameObject>(); // A list of panels representing the word
+
+            foreach (char letter in word)
+            {
+                GameObject panel = Instantiate(letterPrefab);
+                panel.GetComponentInChildren<TextMeshProUGUI>().text = letter.ToString();
+                panel.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+
+                // Indices will be one to one with original phrase
+                solutionPanels.Add(panel);
+
+                wordPanel.Add(panel);
+            }
+
+            // Add a white space to the end of every word
+            GameObject blankPanel = Instantiate(letterPrefab);
+            blankPanel.GetComponentInChildren<TextMeshProUGUI>().text = " ";
+            wordPanel.Add(blankPanel);
+
+            wordPanels.Add(wordPanel); // List of words in panel form
+        }
+
+        //foreach (string word in wordList)
+        //{
+        //    Debug.Log(word);
+        //}
+
+        //foreach (List<GameObject> wordPanel in wordPanels)
+        //{
+        //    foreach (GameObject panel in wordPanel)
+        //    {
+        //        Debug.Log(panel.GetComponent<TextMeshProUGUI>().text);
+        //    }
+        //}
+
+        //foreach (List<GameObject> wordPanel in wordPanels)
+        //{
+        //    foreach (GameObject panel in wordPanel)
+        //    {
+        //        Debug.Log(panel.GetComponent<TextMeshProUGUI>().text);
+        //    }
+        //}
+
+        // Format layout for player by adding padding
+        int rowCount = 1;
+        List<GameObject> panelRow = new List<GameObject>(); // A row of panels in the layout
+
+        for (int i = 0; i < wordPanels.Count; i++)
+        {
+            // Check if row can fit more words
+            if (panelRow.Count + wordPanels[i].Count < 15)
+            {
+                panelRow.AddRange(wordPanels[i]);
+            }
+            // Row filled - pad the rest of the row
+            else if (panelRow.Count + wordPanels[i].Count >= 15)
+            {
+                PadOutRow(panelRow);
+
+                // Create new row
+                //List<GameObject> copy = new List<GameObject>(panelRow);
+                panelLayout.Add(panelRow);
+
+                rowCount++;
+                panelRow = new List<GameObject>(); // A row of panels in the layout
+                i--; // Go back a step
+            }
+        }
+
+        PadOutRow(panelRow);
+
+        // Add last word
+        panelLayout.Add(panelRow);
+        rowCount++;
+
+        //foreach (List<GameObject> row in panelLayout)
+        //{
+        //    foreach (GameObject panel in row)
+        //    {
+        //        Debug.Log(panel.GetComponent<TextMeshProUGUI>().text);
+        //    }
+        //}
+
+        for (int i = 0; i < 2; i++)
+        {
+            panelRow = new List<GameObject>();
+
+            for (int j = 0; j < 15; j++)
+            {
+                GameObject blankPanel = Instantiate(letterPrefab);
+                blankPanel.GetComponentInChildren<TextMeshProUGUI>().text = "";
+
+                panelRow.Add(blankPanel);
+            }
+
+            if (i == 0)
+            {
+                panelLayout.Add(panelRow);
+            }
+            else
+            {
+                panelLayout.Insert(0, panelRow);
+            }
+        }
+    }
+
+    void PadOutRow(List<GameObject> panelRow)
+    {
+        int numPadding = 14 - (panelRow.Count - 1);
+        int padLeft = (int)(numPadding * 0.5f) + 1;
+        int padRight = numPadding - padLeft;
+
+        for (int j = 0; j < padLeft; j++)
+        {
+            GameObject blankPanel = Instantiate(letterPrefab);
+            blankPanel.GetComponentInChildren<TextMeshProUGUI>().text = "";
+
+            panelRow.Insert(0, blankPanel);
+        }
+
+        for (int k = 0; k < padRight; k++)
+        {
+            GameObject blankPanel = Instantiate(letterPrefab);
+            blankPanel.GetComponentInChildren<TextMeshProUGUI>().text = "";
+
+            panelRow.Add(blankPanel);
+        }
+    }
+
+    void PresentPanel()
+    {
+        foreach (List<GameObject> row in panelLayout)
+        {
+            foreach(GameObject panel in row)
+            {
+                panel.transform.parent = transform;
+            }
+        }
+    }
+}
