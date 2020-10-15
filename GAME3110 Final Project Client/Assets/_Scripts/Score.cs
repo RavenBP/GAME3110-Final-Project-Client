23730aa5f2386756c424f7374312d99644327952
Minh Son Tran <tranminhson1989@gmail.com>
2020-10-15 15:04:08 -0400

    Adding score system for player. Adjusting some display colors

---
+++ GAME3110 Final Project Client/Assets/_Scripts/Score.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Point reward system
+public static class Score
+{
+    public const int SINGLEPOINT = 10;
+    public const int WHOLEWORDPOINT = 30;
+}
