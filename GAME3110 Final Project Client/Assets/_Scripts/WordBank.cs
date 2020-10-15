a130a2dafae3ddd1b31729a6ecf5a5c0e3258360
Minh Son Tran <tranminhson1989@gmail.com>
2020-10-15 03:48:10 -0400

    Refactoring WordBank. Showing wrong guess letters. Changing to new guessing round when current word revealed completely.

--- GAME3110 Final Project Client/Assets/_Scripts/WordBank.cs
+++ GAME3110 Final Project Client/Assets/_Scripts/WordBank.cs
@@ -2,22 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct WordPack
+{
+    public string question;
+    public string answer;
+}
+
 // Temporary Words Database
 public class WordBank
 {
-    public string[] Words = new string[10];
+    public WordPack[] Words = new WordPack[10];
 
     public void _InstantiateWorld()
     {
-        Words[0] = "Toronto";
-        Words[1] = "Money";
-        Words[2] = "America";
-        Words[3] = "Sugar";
-        Words[4] = "Wood";
-        Words[5] = "Wallet";
-        Words[6] = "Batman";
-        Words[7] = "Mouse";
-        Words[8] = "House";
-        Words[9] = "BigBang";
+        Words[0].question = "Canada's city";
+        Words[0].answer = "Toronto";
+
+        Words[1].question = "Currency";
+        Words[1].answer = "Money";
+
+        Words[2].question = "A continental";
+        Words[2].answer = "America";
+
+        Words[3].question = "Sweet spice";
+        Words[3].answer = "Sugar";
+
+        Words[4].question = "Popular material";
+        Words[4].answer = "Wood";
+
+        Words[5].question = "Money container";
+        Words[5].answer = "Wallet";
+
+        Words[6].question = "Robin fella";
+        Words[6].answer = "Batman";
+
+        Words[7].question = "Small rodent";
+        Words[7].answer = "Mouse";
+
+        Words[8].question = "Where you live";
+        Words[8].answer = "House";
+
+        Words[9].question = "A theory";
+        Words[9].answer = "Big Bang";
     }
 }
